6082e3ecf071a483b01c270549967ca4a58720e5
Daniel Azami <dazami@stevens.edu>
2021-11-16 15:14:04 -0500

    Most of the way done with Portfolio & Stock models.

--- sdk/models/Stock.py
+++ sdk/models/Stock.py
@@ -14,6 +14,7 @@ class Stock:
         self.price = price
         self.shares = shares
         self.date_purchased = date_purchased
+        self.market_value = price * shares
 
         if description:
             self.description = description
